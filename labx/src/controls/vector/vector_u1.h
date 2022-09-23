565e51c3bbea464c7a2b9a30d67a18718ee4077d
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2022-09-23 11:43:41 -0500

    add control velocity in vector1

--- labx/src/controls/vector/vector_u1.h
+++ labx/src/controls/vector/vector_u1.h
@@ -26,6 +26,18 @@ namespace nature_lab
             {
                 ImGui::Text("position: (%f,%f)", nature_->position.x, nature_->position.y);
                 ImGui::Text("rotation: %f", nature_->rotation);
+                
+                if (ImGui::SliderFloat("velocity x", &this->nature_->velocity_x, 0, 20))
+                {
+                    if (nature_->velocity.x > 0) nature_->velocity.x = nature_->velocity_x;
+                    else nature_->velocity.x = nature_->velocity_x * -1;
+                }
+
+                if (ImGui::SliderFloat("velocity y", &this->nature_->velocity_y, 0, 20))
+                {
+                    if (nature_->velocity.y > 0) nature_->velocity.y = nature_->velocity_y;
+                    else nature_->velocity.y = nature_->velocity_y * -1;
+                }
             }
             end();
         }
